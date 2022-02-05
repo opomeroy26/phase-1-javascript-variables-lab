e016e45205af1b4d70d7296ea626768e0c350eff
Olivia Pomeroy <opomeroy26@gmail.com>
2022-02-05 09:17:51 -0800

    commit tree 8397ca90d5ab21f325cf64581e211d9b9354cb96

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Code your solution in this file!
-const companyName = "Scuber";
+// const companyName = "Scuber";
+// let mostProfitableNeighborhood = "Chelsea";
+// let companyCeo = "Susan Smith";
+
+
+let companyName = "Scuber";
 let mostProfitableNeighborhood = "Chelsea";
 let companyCeo = "Susan Smith";
+
